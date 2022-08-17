598d78d83219e5b1f5dc6743f05564b8d62d641f
Ren <ren.t21@icloud.com>
2022-08-17 22:28:01 +0900

    Initial Commit

---
+++ MarvelAPI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelAPI
+//
+//  Created by 高橋蓮 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by 高橋蓮 on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
